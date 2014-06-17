5671a9d5934da8c9f8da9b992f9e6d8e9cc05f5f
Tobalase Akinyemi <stagnova@gmail.com>
2014-06-17 23:44:30 +0100

    v1.2.0 Secret Sessions Variables now saved in sessions Email addresses to be collected before game starts

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
  ************************************************************************
  *Organisation : Lydraine                                               *
  ************************************************************************
- *Version      : Cookie Cocaine <1.0.0>                                 *
+ *Version      : Secret Session <1.2.0>                                 *
  ************************************************************************
- *Contibutors  : Tobalase Akinyemi <stagnova@gmail.com>                 *
+ *Contributors : Tobalase Akinyemi <stagnova@gmail.com>                 *
  ************************************************************************
  *Summary      : A guessing game that picks a random number between 0   *
  *               and 101 and asks you to guess the number inputing      *
  *               guides to making the right guess with colors to signify*
  *               different alerts.                                      *
+ ************************************************************************
+ *Improvements :Long term variables now stored in sessions.             *
+ *              Email request before game starts.                       *
  ************************************************************************
  *License      : GNU GPLv3 (view below)                                 *
  ************************************************************************
@@ -21,26 +24,42 @@
 
 
 <?php
+// ***PRE-DISPLAY***
 
-// Set cookies to record random number and number of guesses at the
-// detection of no ongoing game play which is identified by presence of
-// cookie.
-if (!isset($_COOKIE['number'])) {
-    Setcookie('number', rand(1, 100), time()+(60*60*24));
-    setcookie('tries', 1, time()+(60*60*24));
+//include('functions.php');
+session_start();
+
+if(isset($_POST['email'])) {
+    // Check for empty email field before starting game play.
+    if($_POST['email'] != "") {
+        $_SESSION['number'] = rand(1, 100);
+        $_SESSION['tries'] = 0;
+        $_SESSION['email'] = $_POST['email'];
+    }    
+}
+
+if(isset($_POST['replay'])) {
+    $_SESSION['number'] = rand(1, 100);
+    $_SESSION['tries'] = 0;
+}
+
+elseif(isset($_POST['exit'])) {
+    $_SESSION = array();
+    setcookie(session_name(), NULL, time()-(60*60*24*7*52*2000));
 }
 ?>
+
 <?php
-// Initially set page background color to purple
+
 $color = "purple";
 
-// Check for invalid input and set error message
+
 if (isset($_POST['guess'])) {
 
-// Check there is an empty input.
+
 if($_POST['guess'] == "") {$errmsg ="Please Enter Your Guess.";}
 
-// Check if guess is a number
+
 elseif(!is_numeric($_POST['guess'])) {$errmsg = "Your guess should be a number!!!"; }
 
 // Check if guess is within the range of 1-100
@@ -58,7 +77,7 @@ else{$color = "purple";}
 if ((isset($_POST['submit']) AND isset($_POST['guess'])) AND !isset($errmsg)) {
 
 // Set number by getting number variable from cookie.
-    $number = $_COOKIE['number'];
+    $number = $_SESSION['number'];
 // Set guess from form input.
     $guess = $_POST['guess'];
 // Set background color to green if guess is correct.
@@ -68,7 +87,7 @@ if ((isset($_POST['submit']) AND isset($_POST['guess'])) AND !isset($errmsg)) {
     else{$color = "maroon";}
 
 // Add one to number of guesses.
-    setcookie('tries', $_COOKIE['tries']+1, time()+(60*60));
+    $_SESSION['tries']++;
 }
 ?>
 
@@ -89,44 +108,58 @@ body{background-color: <?php echo $color; /*echo set background color*/?>;
 <form action = "./index.php" method = "POST">
 <h1>
 <?php
+// ***HEAD***
 // Print greeting message if background color is purple.
-if($color == "purple") {
-echo "<h3>I have chosen a number </em>randomly</em> from 1 to 100</h3>";
-echo "<h2>What Do You Think It Is?</h2>";
+    if(isset($_SESSION['email']) AND $color == "purple") {
+    echo "<h3>I have chosen a number </em>randomly</em> from 1 to 100</h3>";
+    echo "<h2>What Do You Think It Is?</h2>";
 }
 // Print error message if there is any.
 elseif(isset($errmsg)) {echo "Invalid Input". "<br/>" . $errmsg ; }
 
+elseif(isset($_SESSION['email'])) {
+
 // Print "RiGHt" if guess is correct.
-elseif($number == $guess) {echo "RiGHT";}
+if($number == $guess) {echo "RiGHT";}
 
 // Print "wRUNG" if guess is wrong.
 else{echo "wRUNG";}
+}
 ?>
 </h1>
 <br/>
 
 <?php 
-
+// ***INPUT FIELDS***
 // Display input form if guess is incorrect. 
-if ($color != "green") {
-echo "<input name = \"guess\" type = \"text\"><br/>";
-echo "<input name = \"submit\" type =\"submit\" value = \"Enter\">";
+if (isset($_SESSION['email']) AND $color != "green") {
+    echo "<input name = \"guess\" type = \"text\"><br/>";
+    echo "<input name = \"submit\" type =\"submit\" value = \"Enter\">";
 }
 
 // Display Replay button if guess is correct.
+elseif ($color == "green"){
+    echo "<h3>Wanna Play Again?</h3>" . "<br/>";
+    echo "<input name = \"replay\" type = \"submit\" value = \"yes\">";
+    echo "<input name = \"exit\" type = \"submit\" value = \"no\">";
+}
+
+// Request email if it hasn't been entered.
 else{
-echo "<h3>Can You Guess My Number Faster?</h3>";
-echo "<input name = \"replay\" type = \"submit\" value = \"RePlay!!!\">";}
+    echo "<h2>Please Enter Your Email Address</h2>";
+    echo "<input name = \"email\" type = \"text\">" . "<br/>";
+    echo "<input name = \"start\" type = \"submit\" value = \"Start!!!\">";
+}
 ?>
 <h2>
 <?php
-// Unset number cookie when guess is correct.
-// Print number of guesses and unset the related cookie when guess is correct.  
+// ***SUB-MESSAGES***
+// Unset number session variable when guess is correct.
+// Print number of guesses and unset the related session variable when guess is correct.  
 if(($number == $guess) AND ($color == "green")) {
-echo "It took you " . $_COOKIE['tries'] . " guesses.";
-setcookie('tries', NULL, time()+(60*60*24));
-setcookie('number', NULL, time()-(60*60*24));
+echo "It took you " . $_SESSION['tries'] . " guesses.";
+unset($_SESSION['tries']);
+unset($_SESSION['number']);
 }
 
 // If guess is incorrect, print guide.
@@ -136,5 +169,6 @@ elseif($number < $guess) {echo "Go Lower";}
 </h2>
 </form>
 
+<?php /*print_r($_SESSION)*/;?>
 </body>
 </html>	
